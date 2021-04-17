5c63572e0c860cdd775fdbd06d3a5a2accf63711
kkolodziej-dev <frostbolty@gmail.com>
2021-04-17 21:00:34 +0200

    add test photos

--- assets/js/documents.js
+++ assets/js/documents.js
@@ -1,31 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const DOCUMENT_FOLDER_PATH = 'https://github.com/kkolodziej-dev/imcspoland/raw/add_galery/images/documents/';
+    const PHOTO_FOLDER_PATH = 'https://github.com/kkolodziej-dev/imcspoland/raw/add_galery/images/gallery/';
     //ToDo get list of available documents to loop through
     const DOCUMENT_NAMES_ARRAY = ['document1.pdf', 'document1.docx'];
-    
+    const PHOTO_NAMES_ARRAY = ['01.jpg','02.jpg','03.jpg','04.jpg','05.jpg','06.jpg'];
+
     let documentListElement = document.getElementById('documentList');
-    let documentsArray = [];
+    let photoListElement = document.getElementById('photoList');
+
+    //populate documents
+    populateElementFields('documents');
+    populateElementFields('gallery');
     
-    //construct the documents
-    DOCUMENT_NAMES_ARRAY.forEach(e => {
-        let tempSpan = document.createElement("span");
-        tempSpan.className = "icon minor style6 fad fa-file-alt";
-        tempSpan.innerText = e;
-        setEventListeners(tempSpan);
-        let tempLi = document.createElement("li");
-        tempLi.appendChild(tempSpan);
-        documentsArray.push(tempLi);
-    })
+    function populateElementFields(flag) {
+        let folderPath, elementList;
+        switch (flag) {
+            case 'documents':
+                folderPath = DOCUMENT_FOLDER_PATH;
+                elementList = DOCUMENT_NAMES_ARRAY;
+                break;
+            case 'gallery':
+                folderPath = PHOTO_FOLDER_PATH;
+                elementList = PHOTO_NAMES_ARRAY;
+                break;
+        }
+        let elementArray = [];
+        //construct the documents
+        elementList.forEach(e => {
+            let tempSpan = document.createElement("span");
+            tempSpan.className = "icon minor style6 fad fa-file-alt";
+            tempSpan.innerText = e;
+            setEventListeners(tempSpan, folderPath);
+            let tempLi = document.createElement("li");
+            tempLi.appendChild(tempSpan);
+            elementArray.push(tempLi);
+        })
+
+        //append ready document objects to the document list element
+        elementArray.forEach(e => {
+            switch (flag) {
+                case 'documents':
+                    documentListElement.appendChild(e);
+                    break;
+                case 'gallery':
+                    photoListElement.appendChild(e);
+                    break;
+            } 
+        })
+    }
     
-    //append ready document objects to the document list element
-    documentsArray.forEach(e => {
-        documentListElement.appendChild(e);
-    })
-    
-    function setEventListeners(e) {
+    function setEventListeners(e, folderPath) {
         e.addEventListener('click', () =>  {
-            download(e.innerText);
+            download(folderPath, e.innerText);
         });
         
         e.addEventListener('mouseover', () => {
@@ -33,9 +60,9 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
     
-    function download(documentName) {
+    function download(folderPath, elementName) {
         let element = document.createElement('a');
-        element.setAttribute('href', DOCUMENT_FOLDER_PATH + documentName);
+        element.setAttribute('href', folderPath + elementName);
         element.style.display = 'none';
         document.body.appendChild(element);
         element.click();
